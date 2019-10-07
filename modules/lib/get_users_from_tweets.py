713820f383b0a4c4b90ea8a6096902c13af9fe9f
hugrave <edoardo.conte@live.it>
2019-10-07 20:57:31 +0200

    Neo4j support added

--- modules/lib/get_users_from_tweets.py
+++ modules/lib/get_users_from_tweets.py
@@ -13,11 +13,10 @@ from ..neo4j.interface import Neo4j
 
 class GetUser:
 
-    def __init__(self, users_file, geocode="34.036654,-118.193582,150km"):
+    def __init__(self, geocode="34.036654,-118.193582,150km"):
         self.twitter = TwitterAuth()
         self.neo4j = Neo4j()
         self.geocode = geocode
-        self.users_file = users_file
 
 
     def run(self, initial_run=False):
@@ -35,9 +34,8 @@ class GetUser:
                 last_id = self.neo4j.get_lastId()
                 try:
                     tweets = self.twitter.get_tweets(self.geocode, last_id=last_id)
-                except AssertionError as e:
-                    print(e)
-                    return False
+                except:
+                    return
 
             last_id = self.get_max_id(tweets)
 
@@ -45,9 +43,8 @@ class GetUser:
             self.neo4j.write_lastId(last_id)
 
             #Save the extracted users
-            user_ids = [tweet['user']['id_str']+'\n' for tweet in tweets['statuses']]
-            with open(self.users_file, 'a') as myfile:
-                myfile.writelines(user_ids)
+            users = [tweet['user'] for tweet in tweets]
+            self.neo4j.write_users(users)
 
         
     
--- modules/neo4j/interface.py
+++ modules/neo4j/interface.py
@@ -1,17 +1,31 @@
+from neo4j import GraphDatabase
+import time
+
 class Neo4j:
-    def __init__(self, ):
+    def __init__(self, uri, user, password):
         """
         Opening the session to Neo4j
         """
-        pass
+        self._driver = GraphDatabase.driver(uri, auth=(user, password))
 
+    def close(self, ):
+        self._driver.close()
+
+    
     def init_execution(self, ):
         """
         Write some stats regarding the initial time of execution
         This is useful if we finish the limit of 7 days,
         so we know at which time we started 
         """
-        pass
+        now = int(round(time.time() * 1000))
+
+        with self._driver.session() as session:
+            session.run(
+                "create (node:Execution {startedAt: {now}})",
+                now=now
+            )
+    
 
     def get_lastId(self, ):
         """
@@ -24,3 +38,15 @@ class Neo4j:
         Update the value for the last_id
         """
         pass
+
+
+if __name__ == "__main__":
+    neo4j = Neo4j(
+        uri="bolt://localhost:7687",
+        user="neo4j",
+        password="neo4j",
+    )
+
+    neo4j.init_execution()
+
+    neo4j.close()
